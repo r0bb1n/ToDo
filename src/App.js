be31fb2be70696e1ef636495eeebbfbc8a8d31c7
r0bb1n <kjellgrenrobin@gmail.com>
2022-05-28 15:50:15 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
 
-function App() {
+
+export default function App () {
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+
+    if (savedTodos) {
+      return JSON.parse(savedTodos)
+    } else {
+      return []
+    }
+  });
+  const [todo, setTodo] = useState("");
+
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos]);
+
+  function handleInputChange(e) {
+    setTodo(e.target.value);
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+
+    if (todo !== "") {
+      setTodos([
+        ...todos,
+        {
+          id: todos.length + 1,
+          text: todo.trim()
+        }
+      ])
+    }
+
+    setTodo("");
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <form onSubmit={handleFormSubmit}>
+        <input
+        name="todo"
+        type="text"
+        placeholder="Create a new Todo"
+        value={todo}
+        onChange={handleInputChange}
+         />
+      </form>
 
-export default App;
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
